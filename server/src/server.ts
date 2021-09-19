e7d73a4604da5c3df7aa14766de5e4dc07d8460e
angusfng <angusfeng99@gmail.com>
2021-09-19 20:52:36 +1000

    basic login

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { query } from "../db";
 import { MyDatabaseError } from "./error";
 
@@ -17,6 +18,15 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Memories server");
 });
+
+// Auth functions
+const getUsernameFromToken = (authorization: string) => {
+  const token = authorization.replace("Bearer ", "");
+  const { username } = jwt.verify(token, process.env.JWT_SECRET!) as {
+    username: string;
+  };
+  return username;
+};
 
 app.post("/register", async (req, res) => {
   if (req.body.password !== req.body.confirmPassword) {
@@ -45,15 +55,27 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   // Get user
-  const text = "select password from users where username = $1";
-  const response = await query(text, [req.body.username]);
-  console.log(response.rows);
-  if (response.rows.length === 0) {
-    return res.status(400).json({ error: "Wrong username or password" });
+  try {
+    const text = "select password from users where username = $1";
+    const response = await query(text, [req.body.username]);
+    if (response.rows.length === 0) {
+      return res.status(400).json({ error: "User doesn't exist" });
+    }
+    const passwordHash = response.rows[0].password;
+    const compare = await bcrypt.compare(req.body.password, passwordHash);
+    if (compare) {
+      const token = await jwt.sign(
+        { username: req.body.username },
+        process.env.JWT_SECRET!
+      );
+      console.log(getUsernameFromToken(`Bearer ${token}`));
+      return res.status(200).json({ message: "Logged In", token: token });
+    }
+  } catch (err) {
+    console.log(err);
   }
-  return res.status(200).json({ message: "Logged In" });
-  // Compare passwords
-  // JWT
+
+  return res.status(400).json({ error: "Wrong password" });
 });
 
 app.listen(port, () => {
